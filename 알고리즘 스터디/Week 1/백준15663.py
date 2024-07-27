295f421f8341c53939e43d0755596c92dcabdebe
luhayang <luhayang@Luhas-Laptop.local>
2024-07-27 09:11:48 -0500

    2661

---
+++ 알고리즘 스터디/Week 1/백준15663.py
@@ -0,0 +1,11 @@
+import sys
+
+def permutation(n, m, num):
+    perm = []
+    
+
+if __name__ == "__main__":
+    n, m = map(int, sys.stdin.readline().split())
+    num = list(map(int, sys.stdin.readline().split()))
+    
+    print(num)
---
+++ 알고리즘 스터디/Week 1/백준2661.py
@@ -0,0 +1,24 @@
+import sys
+
+n = int(sys.stdin.readline())
+result = []
+
+def check():
+    length = len(result)
+    for i in range(1, length//2 +1):
+        if result[length-i:] == result[length - 2*i : length-i]:
+            return False
+    return True
+
+def backTracking(idx):
+    if idx == n:
+        print(*result, sep='')
+        sys.exit(0)
+
+    for i in range(1, 4):
+        result.append(i)
+        if check():
+            backTracking(idx+1)
+        result.pop()
+
+backTracking(0)
